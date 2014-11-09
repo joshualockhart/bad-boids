f5f92a0d3315c4a00f9e03a768dbf8449109f84b
joshualockhart <joshualockhart@gmail.com>
2014-11-09 13:28:44 +0000

    removed magic numbers

--- boids.py
+++ boids.py
@@ -9,10 +9,28 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+x_pos_bound = (-450,50.0)
+y_pos_bound = (300.0,600.0)
+x_velocity_bound = (0,10.0)
+y_velocity_bound = (-20.0,20.0)
+
+num_boids = 50
+
+dt_middle = 0.01
+dt_match = 0.125
+clumping = 100
+speed_match = 10000
+
+xlim=(-500,1500)
+ylim=(-500,1500)
+
+frames = 50
+interval = 50
+
+boids_x=[random.uniform(*x_pos_bound) for x in range(num_boids)]
+boids_y=[random.uniform(*y_pos_bound) for x in range(num_boids)]
+boid_x_velocities=[random.uniform(*x_velocity_bound) for x in range(num_boids)]
+boid_y_velocities=[random.uniform(*y_velocity_bound) for x in range(num_boids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
@@ -20,22 +38,22 @@ def update_boids(boids):
 	# Fly towards the middle
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*dt_middle/len(xs)
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*dt_middle/len(xs)
 	# Fly away from nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < clumping:
 				xvs[i]=xvs[i]+(xs[i]-xs[j])
 				yvs[i]=yvs[i]+(ys[i]-ys[j])
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < speed_match:
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*dt_match/len(xs)
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*dt_match/len(xs)
 	# Move according to velocities
 	for i in range(len(xs)):
 		xs[i]=xs[i]+xvs[i]
@@ -43,7 +61,7 @@ def update_boids(boids):
 
 
 figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
+axes=plt.axes(xlim=xlim, ylim=ylim)
 scatter=axes.scatter(boids[0],boids[1])
 
 def animate(frame):
@@ -52,7 +70,7 @@ def animate(frame):
 
 
 anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+                               frames=frames, interval=interval)
 
 if __name__ == "__main__":
     plt.show()
